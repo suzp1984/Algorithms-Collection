011416e1e8af2c1efa0e98a7d1e6fc938a207491
suzp1984 <suzp1984@gmail.com>
2016-09-05 15:06:01 +0800

    add Quick Union implementation

---
+++ java/algorithms/src/main/java/io/github/suzp1984/algorithms/QuickUnionConnection.java
@@ -0,0 +1,96 @@
+package io.github.suzp1984.algorithms;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class QuickUnionConnection<T> implements IDynamicConnection<T> {
+
+	private final List<Item> mDataSets = new LinkedList<>();
+	
+	public void add(T t) {
+		if (findItem(t) == null) {
+			mDataSets.add(new Item<T>(t));
+		}
+	}
+	
+	@Override
+	public void union(T a, T b) {
+		// TODO Auto-generated method stub
+		addItem(a);
+		addItem(b);
+		
+		Item roota = getRoot(findItem(a));
+		Item rootb = getRoot(findItem(b));
+		
+		if (roota == null) {
+			roota = new Item(a);
+			mDataSets.add(roota);
+		}
+		
+		if (rootb == null) {
+			rootb = new Item(b);
+			mDataSets.add(rootb);
+		}
+		
+		rootb.parent = roota;
+	}
+
+	@Override
+	public boolean isConnected(T a, T b) {
+		
+		Item roota = getRoot(findItem(a));
+		Item rootb = getRoot(findItem(b));
+		
+		if (roota != null && rootb != null && roota.equals(rootb)) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	private void addItem(T t) {
+		if (findItem(t) == null) {
+			mDataSets.add(new Item<T>(t));
+		}
+	}
+	
+	private Item findItem(T t) {
+		for (Item i : mDataSets) {
+			if (i.t.equals(t)) {
+				return i;
+			}
+		}
+		
+		return null;
+	}
+
+	private Item getRoot(Item t) {
+		if ( t == null) {
+			return null;
+		}
+		
+		while (t.parent != null) {
+			t = t.parent;
+		}
+		
+		return t;
+	}
+	
+	public static class Item<T> {
+		T t;
+		Item parent;
+		
+		public Item(T t) {
+			this.t = t;
+			parent = null;
+		}
+		
+		public boolean isRoot() {
+			return parent == null;
+		}
+		
+		public Item parent() {
+			return parent;
+		}
+	}
+}
---
+++ java/algorithms/src/test/java/io/github/suzp1984/algorithms/QuickUnionConnectionTest.java
@@ -0,0 +1,22 @@
+package io.github.suzp1984.algorithms;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class QuickUnionConnectionTest {
+	 @Test
+	 public void testInt() {
+		 QuickUnionConnection<Integer> intQU = new QuickUnionConnection<>();
+		 
+		 for (int i = 0; i < 10; i++) {
+			 intQU.add(i);
+		 }
+		 
+		 intQU.union(2, 3);
+		 intQU.union(3, 4);
+		 intQU.union(2, 7);
+		 
+		 assertEquals(true, intQU.isConnected(4, 7));
+	 }
+}
