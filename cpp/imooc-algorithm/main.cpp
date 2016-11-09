4f242092ef48c2ee2f1da74c260e6f9be09d48d3
suzp1984 <suzp1984@gmail.com>
2016-11-09 23:48:06 +0800

    add test case

--- cpp/imooc-algorithm/main.cpp
+++ cpp/imooc-algorithm/main.cpp
@@ -19,7 +19,32 @@ int main() {
 
     selectionSort(a, 10);
     for (int i = 0; i < 10; i++) {
-        cout << a[i] << " " << endl;
+        cout << a[i] << " ";
     }
+    cout << endl;
+
+    float f[4] = {4.2, 3.2, 5.1, 1.0};
+
+    selectionSort(f, 4);
+    for (int i = 0; i < 4; ++i) {
+        cout << f[i] << " ";
+    }
+    cout << endl;
+
+    string c[4] = {"B", "C", "A", "D"};
+    selectionSort(c, 4);
+
+    for (int i = 0; i < 4; ++i) {
+        cout << c[i] << " ";
+    }
+    cout << endl;
+
+    student s[4] = {{"D", 23}, {"A", 11},  {"C", 49}, {"B", 41}};
+    selectionSort(s, 4);
+    for (int i = 0; i < 4; ++i) {
+        cout << s[i];
+    }
+    cout << endl;
+
     return 0;
 }
